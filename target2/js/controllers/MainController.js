a007228f1b663a9ba941cf94788a964b5fb511e3
Instructor <jsitbon@qcc.cuny.edu>
2018-12-05 10:06:15 -0500

    end of class

--- target2/js/controllers/MainController.js
+++ target2/js/controllers/MainController.js
@@ -27,5 +27,20 @@ app.controller('MainController', ['$scope', function($scope) {
     price: 1.99
   };
 
+  $scope.apps = [
+  {
+   icon: 'img/move.jpg',
+   title: 'MOVE',
+   developer: 'MOVE, Inc.',
+   price: 0.99
+  },
+  {
+    icon: 'img/shutterbugg.jpg',
+    title: 'Shutterbugg',
+    developer: 'Chico Dusty',
+    price: 2.99
+  }
+];
+
 
 }]);
