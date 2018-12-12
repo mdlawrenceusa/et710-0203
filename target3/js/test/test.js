4788141b682149f4d4ef041ab5c454ffed5db8a9
Instructor <jsitbon@qcc.cuny.edu>
2018-12-12 09:47:13 -0500

    complete with errors

--- target3/js/test/test.js
+++ target3/js/test/test.js
@@ -1,20 +1,11 @@
-describe('directives', function(){
+describe('Did you create a new service named forecast?', function() {
+  var ctrl, scope, service;
 
-  beforeEach(module('AppMarketApp'));
-  beforeEach(module('js/directives/appInfo.html'));
+  beforeEach(module('ForecastApp'));
 
-  beforeEach(inject(function($rootScope, $compile, $injector) {
-    elm = angular.element("<app-info info='info'></app-info>");
-
-    scope = $rootScope;
-    scope.info = {icon: "https://www.google.com/images/srpr/logo11w.png", title: "hello", developer: "kelvin", price: 19};
-
-    $compile(elm)(scope);
-    scope.$digest();
-  }));
-
-  it('Did you make a directive named appInfo?', inject(function($controller) {
-    expect(elm.hasClass('ng-isolate-scope')).toBeTruthy();
-  }));
-
+  it('Did you create a new service named forecast?', function() {
+    inject(function($controller, $rootScope, forecast) {
+      expect(forecast).toBeDefined();
+    });
+  });
 });
